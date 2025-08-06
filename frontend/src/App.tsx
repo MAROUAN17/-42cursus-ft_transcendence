a04009977f150cceade0f68d3f40ac4cde4e31ca
MAROUAN17 <marinosaglagal25@gmail.com>
2025-08-06 13:55:28 +0100

    add routing for game and user

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Game from './components/game'
+import { Routes, Route } from 'react-router-dom';
+import Game from './components/game/game'
+import Login from "./components/user/login"
 
 export default function App() {
   return (
     <div>
-      <Game />
+      <Routes>
+        <Route path='/game' element={<Game />}/>
+        <Route path='/login' element={<Login />}/>
+      </Routes>
     </div>
   )
 }
