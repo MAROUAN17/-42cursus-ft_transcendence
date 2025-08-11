26c5730c3f824746649840067e952ad8b23a5b5b
MAROUAN17 <marinosaglagal25@gmail.com>
2025-08-11 12:31:07 +0100

    store + check hashed password

--- backend/src/services/login.service.ts
+++ backend/src/services/login.service.ts
@@ -1,10 +1,12 @@
 import app from "../server.js";
 import { type User, type LoginBody  } from "../models/user.js"
 import type { FastifyReply, FastifyRequest } from "fastify";
+import bcrypt from "bcrypt";
 
 export const loginUser = async (req: FastifyRequest<{Body: LoginBody}>, res: FastifyReply) => {
     try {
         let user = {} as User | undefined;
+        let userPass = "" as string;
         const { username, email, password } = req.body;
 
 
@@ -13,25 +15,36 @@ export const loginUser = async (req: FastifyRequest<{Body: LoginBody}>, res: Fas
             user = app.db
                 .prepare('SELECT * from players WHERE username = ?')
                 .get(username) as User | undefined;
+
+            userPass = app.db
+                .prepare('SELECT password from players WHERE username = ?')
+                .get(username);
         }
         //check email
         if (email) {
             user = app.db
                 .prepare('SELECT * from players WHERE email = ?')
                 .get(email) as User | undefined;
+
+            userPass = app.db
+                .prepare('SELECT password from players WHERE email = ?')
+                .get(email);
         }
-        
-        
-        if (!user) {
-            console.log(user);
-            res.status(404).send({ error: "User not found"});
+
+        console.log(userPass.password);
+        const isMatch = bcrypt.compareSync(password, userPass.password);
+
+        if (!user || !isMatch) {
+            return res.status(500).send({ error: "Wrong credentials"});
         }
 
         //verify JWT token
         const token = app.jwt.sign({ email, username });
+        
+        return res.status(200).send({ token: token, message: "Logged in" });
 
-        return res.status(200).send({ token: token });
-    } catch (error) {
-        console.log(error);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({ err });
     }
 }
--- backend/src/services/register.service.ts
+++ backend/src/services/register.service.ts
@@ -12,15 +12,19 @@ export const registerUser = async (req: FastifyRequest<{Body: LoginBody}>, res: 
             .prepare('SELECT * from players WHERE username = ? AND email = ?')
             .get(username, email) as User | undefined;
 
-    //hashing the password
     if (user) {
         return res.status(500).send({ error: "User already exist!" });
     }
 
+    //hashing the password
+    const hash = bcrypt.hashSync(password, 10);
+    if (!hash)
+        res.status(500).send({ error: "Password hashing failed" });
+
     //register if not exists
     const info = app.db
         .prepare('INSERT INTO players(username, email, password) VALUES (?, ?, ?)')
-        .run(username, email, password);
+        .run(username, email, hash);
 
     return res.status(200).send({ message: info.changes });
 }
