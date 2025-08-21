6d87f0e2d254b13145f864be638522427898ff05
MAROUAN17 <marinosaglagal25@gmail.com>
2025-08-21 16:57:15 +0100

    update the logic of refresh token (the access token should be checked first)

--- backend/src/plugins/jwt.plugin.ts
+++ backend/src/plugins/jwt.plugin.ts
@@ -7,12 +7,12 @@ import { type userInfos } from "../models/user.model.js";
 export const jwtPlugin = fp(async function(fastify, opts) {
     app.decorate('jwtAuth', async function(req: FastifyRequest, res: FastifyReply): Promise<any> {
         try {
-            const refreshToken = req.cookies.refreshToken;
-            await app.jwt.jwt2.verify(refreshToken);
+            const accessToken = req.cookies.accessToken;
+            await app.jwt.jwt1.verify(accessToken);
+        } catch (err) {
             try {
-                const OldAccessToken = req.cookies.accessToken;
-                await app.jwt.jwt1.verify(OldAccessToken);
-            } catch (error) {
+                const refreshToken = req.cookies.refreshToken;
+                await app.jwt.jwt2.verify(refreshToken);
                 const infos = app.jwt.jwt2.decode(refreshToken!) as userInfos | undefined;
                 const newAccessToken = app.jwt.jwt1.sign({ email: infos?.email, username: infos?.username }, { expiresIn: '10s' });
                 res.status(200).setCookie('accessToken', newAccessToken, {
@@ -22,10 +22,9 @@ export const jwtPlugin = fp(async function(fastify, opts) {
                     sameSite: 'lax',
                     maxAge: 10
                 }).send({ message: "another access token generated!" });
+            } catch (err: any) {
+                res.code(401).send({ error: err.message });
             }
-
-        } catch (error: any) {
-            res.code(401).send({ error: error.message });
         }
     })
 });
