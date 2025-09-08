b63aaab5b153bd8b0489737f687bd4dfac225da4
Geesama02 <oussama.thecreative@gmail.com>
2025-09-08 15:59:22 +0100

    Block works in realtime chat

--- frontend/src/components/chat/chat.tsx
+++ frontend/src/components/chat/chat.tsx
@@ -5,19 +5,13 @@ import { IoIosMore } from "react-icons/io";
 import { RiSendPlaneFill } from "react-icons/ri";
 import UserBubble from "./UserBubble";
 import { IoCheckmark } from "react-icons/io5";
-import type { User } from "../../../../backend/src/models/user.model";
+import type { User, userInfos } from "../../../../backend/src/models/user.model";
 import { useState, useEffect, useRef } from "react";
-import type {
-  UsersLastMessage,
-  messagePacket,
-} from "../../../../backend/src/models/chat";
+import type { UsersLastMessage, messagePacket } from "../../../../backend/src/models/chat";
 import ChatBubble from "./chatBubble";
 import { v4 as uuidv4 } from "uuid";
 import { useWebSocket } from "./websocketContext";
-import type {
-  notificationPacket,
-  websocketPacket,
-} from "../../../../backend/src/models/webSocket.model";
+import type { notificationPacket, websocketPacket } from "../../../../backend/src/models/webSocket.model";
 import { useParams, useNavigate } from "react-router";
 import { MdBlock } from "react-icons/md";
 import api from "../../axios";
@@ -25,14 +19,16 @@ import api from "../../axios";
 const Chat = () => {
   const { username } = useParams<{ username?: string }>();
   const [show, setShow] = useState<boolean>(false);
-  const [blocked, setBlocked] = useState<boolean>(false);
+  const [blockedbyUser, setBlockedByUser] = useState<boolean>(false);
+  const [blockedbyOther, setBlockedByOther] = useState<boolean>(false);
   const [isOptionsOpen, setIsOptionsOpen] = useState<boolean>(false);
   const [messages, setMessages] = useState<messagePacket[]>([]);
   const [users, setUsers] = useState<UsersLastMessage[]>([]);
-  const [targetUser, setTargetUser] = useState<User | null>();
-  const [currUser, setCurrUser] = useState<User>();
+  const [targetUser, setTargetUser] = useState<userInfos | null>();
+  const [currUser, setCurrUser] = useState<userInfos>();
   const [searchInput, setSearchInput] = useState<string>("");
   const currUserRef = useRef(currUser);
+  const blockedbyOtherRef = useRef(blockedbyOther);
   const targetUserRef = useRef(targetUser);
   const navigate = useNavigate();
 
@@ -77,17 +73,16 @@ const Chat = () => {
           return 1;
         });
         if (username) {
-          const foundUser: UsersLastMessage = res.data.data.find(
-            (user: UsersLastMessage) => {
-              return user.user.username == username;
-            }
-          );
+          const foundUser: UsersLastMessage = res.data.data.find((user: UsersLastMessage) => {
+            return user.user.username == username;
+          });
           if (foundUser) {
-            res.data.data.map((user: UsersLastMessage) =>
-              user.user.id == foundUser.user.id ? (user.unreadCount = 0) : null
-            );
+            res.data.data.map((user: UsersLastMessage) => (user.user.id == foundUser.user.id ? (user.unreadCount = 0) : null));
             setTargetUser(foundUser.user);
             targetUserRef.current = foundUser.user;
+            setBlockedByUser(foundUser.blockedByUser);
+            setBlockedByOther(foundUser.blockedByOther);
+            blockedbyOtherRef.current = foundUser.blockedByOther;
           }
         }
         setUsers(res.data.data);
@@ -98,8 +93,7 @@ const Chat = () => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          const messageObj: string | null =
-            entry.target.getAttribute("data-message");
+          const messageObj: string | null = entry.target.getAttribute("data-message");
           if (!messageObj) return;
           const msgPacket: messagePacket = JSON.parse(messageObj);
           if (!msgPacket.id) return;
@@ -109,11 +103,7 @@ const Chat = () => {
             data: msgPacket,
           };
           send(JSON.stringify(socketpacket));
-          setMessages((prev) =>
-            prev.filter((msg) =>
-              msg.id === msgPacket.id ? (msg.isRead = true) : msg
-            )
-          );
+          setMessages((prev) => prev.filter((msg) => (msg.id === msgPacket.id ? (msg.isRead = true) : msg)));
           observer.unobserve(entry.target);
         }
       });
@@ -127,10 +117,7 @@ const Chat = () => {
     if (packet.type != "chat") return;
     const newMsg: messagePacket = packet.data;
     if (newMsg.type === "message") {
-      if (
-        newMsg.recipient_id == currUserRef.current?.id &&
-        newMsg.sender_id == targetUserRef.current?.id
-      ) {
+      if (newMsg.recipient_id == currUserRef.current?.id && newMsg.sender_id == targetUserRef.current?.id) {
         setMessages((prev) => [newMsg, ...prev]);
       }
       setUsers((prev: UsersLastMessage[]) => {
@@ -140,11 +127,7 @@ const Chat = () => {
           ...prev[index],
           lastMessage: newMsg,
           unreadCount:
-            prev[index].unreadCount +
-            (newMsg.recipient_id == currUserRef.current?.id &&
-            newMsg.sender_id == targetUserRef.current?.id
-              ? 0
-              : 1),
+            prev[index].unreadCount + (newMsg.recipient_id == currUserRef.current?.id && newMsg.sender_id == targetUserRef.current?.id ? 0 : 1),
         };
         const copy = [...prev];
         copy.splice(index, 1);
@@ -154,16 +137,11 @@ const Chat = () => {
     } else if (newMsg.type == "markSeen") {
       setMessages((prev) => {
         return prev.map((msg: messagePacket) => {
-          return msg.id && msg.id == newMsg.id
-            ? { ...msg, isRead: true, isDelivered: true }
-            : msg;
+          return msg.id && msg.id == newMsg.id ? { ...msg, isRead: true, isDelivered: true } : msg;
         });
       });
       setUsers((prev: UsersLastMessage[]) => {
-        const index = prev.findIndex(
-          (u) =>
-            u.user.id === newMsg.recipient_id && u.lastMessage?.id == newMsg.id
-        );
+        const index = prev.findIndex((u) => u.user.id === newMsg.recipient_id && u.lastMessage?.id == newMsg.id);
         if (index == -1) return prev;
         if (prev[index].lastMessage) prev[index].lastMessage!.isRead = true;
         return prev;
@@ -171,17 +149,11 @@ const Chat = () => {
     } else if (newMsg.type == "markDelivered") {
       setMessages((prev) => {
         return prev.map((msg: messagePacket) => {
-          return msg.tempId == newMsg.tempId
-            ? { ...msg, id: newMsg.id, isDelivered: true }
-            : msg;
+          return msg.tempId == newMsg.tempId ? { ...msg, id: newMsg.id, isDelivered: true } : msg;
         });
       });
       setUsers((prev: UsersLastMessage[]) => {
-        const index = prev.findIndex(
-          (u) =>
-            u.user.id === newMsg.recipient_id &&
-            u.lastMessage?.tempId == newMsg.tempId
-        );
+        const index = prev.findIndex((u) => u.user.id === newMsg.recipient_id && u.lastMessage?.tempId == newMsg.tempId);
         if (index == -1) return prev;
         if (prev[index].lastMessage) {
           prev[index].lastMessage!.id = newMsg.id;
@@ -189,7 +161,33 @@ const Chat = () => {
         }
         return prev;
       });
+    } else if (newMsg.type == "block") {
+      setBlockedByOther(!blockedbyOtherRef.current);
+      blockedbyOtherRef.current = !blockedbyOtherRef.current;
+      setUsers((prev: UsersLastMessage[]) => {
+        return prev.map((user) => {
+          return user.user.id === newMsg.recipient_id ? { ...user, blockedByOther: !user.blockedByOther } : user;
+        });
+      });
     }
+  }
+
+  function blockUser() {
+    if (!targetUser || !currUser) return;
+    const msgPacket: messagePacket = {
+      type: "block",
+      isDelivered: false,
+      sender_id: currUser.id,
+      recipient_id: targetUser.id,
+      message: "",
+      isRead: false,
+      createdAt: new Date().toISOString().replace("T", " ").split(".")[0],
+    };
+    const socketPacket: websocketPacket = {
+      type: "chat",
+      data: msgPacket,
+    };
+    send(JSON.stringify(socketPacket));
   }
 
   function updateNotification() {
@@ -226,9 +224,7 @@ const Chat = () => {
           createdAt: new Date().toISOString().replace("T", " ").split(".")[0],
         };
         setUsers((prev: UsersLastMessage[]) => {
-          const index = prev.findIndex(
-            (u) => u.user.id === msgPacket.recipient_id
-          );
+          const index = prev.findIndex((u) => u.user.id === msgPacket.recipient_id);
           if (index == -1) return prev;
           const updatedUser: UsersLastMessage = {
             ...prev[index],
@@ -282,16 +278,13 @@ const Chat = () => {
                   unreadCount={user.unreadCount}
                   isRead={user.lastMessage?.isRead}
                   isDelivered={user.lastMessage?.isDelivered}
-                  type={
-                    user.lastMessage
-                      ? user.lastMessage.sender_id == currUser?.id
-                        ? "sender"
-                        : "recipient"
-                      : "recipient"
-                  }
+                  type={user.lastMessage ? (user.lastMessage.sender_id == currUser?.id ? "sender" : "recipient") : "recipient"}
                   onclick={() => {
                     setTargetUser(user.user);
                     targetUserRef.current = user.user;
+                    setBlockedByUser(user.blockedByUser);
+                    setBlockedByOther(user.blockedByOther);
+                    blockedbyOtherRef.current = user.blockedByOther;
                     user.unreadCount = 0;
                     updateNotification();
                   }}
@@ -304,14 +297,10 @@ const Chat = () => {
                   }
                   name={user.user.username}
                   style={`transform h-[85px] ${
-                    user.user != targetUser
-                      ? "hover:scale-105 hover:bg-neon/[35%]"
-                      : "scale-105 bg-neon/[35%]"
+                    user.user != targetUser ? "hover:scale-105 hover:bg-neon/[35%]" : "scale-105 bg-neon/[35%]"
                   } transition duration-300 flex flex-row p-5 gap-3 items-center bg-compBg/[25%] rounded-xl`}
                 />
-                {i + 1 < arr.length ? (
-                  <hr className="border-t border-[0.5px] border-[#76767C] my-[6px] mx-6 rounded-full" />
-                ) : null}
+                {i + 1 < arr.length ? <hr className="border-t border-[0.5px] border-[#76767C] my-[6px] mx-6 rounded-full" /> : null}
               </div>
             ))}
         </div>
@@ -321,14 +310,9 @@ const Chat = () => {
           <>
             <div className="bg-compBg/20 h-[95px] items-center flex px-7 justify-between ">
               <div className="flex gap-3 items-center">
-                <img
-                  src="/src/assets/photo.png"
-                  className="h-[44px] w-[44px]"
-                />
+                <img src="/src/assets/photo.png" className="h-[44px] w-[44px]" />
                 <div className="flex flex-col">
-                  <h3 className="font-semibold text-white">
-                    {targetUser.username}
-                  </h3>
+                  <h3 className="font-semibold text-white">{targetUser.username}</h3>
                   <div className="flex gap-1">
                     <div className="w-[9px] h-[9px] rounded-full bg-green-600 mt-2"></div>
                     <p className="text-[#BABABA]">Online</p>
@@ -336,10 +320,7 @@ const Chat = () => {
                 </div>
               </div>
               <div className="relative">
-                <IoIosMore
-                  onClick={() => setIsOptionsOpen(!isOptionsOpen)}
-                  className="text-white h-10 w-10 hover:bg-compBg/30 rounded-full p-2"
-                />
+                <IoIosMore onClick={() => setIsOptionsOpen(!isOptionsOpen)} className="text-white h-10 w-10 hover:bg-compBg/30 rounded-full p-2" />
                 {isOptionsOpen ? (
                   <div className="absolute overflow-hidden right-0 mt-2 w-fit z-10 bg-[#1f085f] border-2 border-neon/10 rounded-lg shadow-[0_0px_1px_rgba(0,0,0,0.25)] shadow-neon">
                     <ul className="">
@@ -347,15 +328,12 @@ const Chat = () => {
                         <FaUser className="text-white" />
                         Profile
                       </li>
-                      {blocked ? (
+                      {blockedbyUser ? (
                         <li
                           onClick={() => {
-                            setBlocked(false);
-                            api.post(
-                              "/unblock/" + targetUser.id,
-                              {},
-                              { withCredentials: true }
-                            );
+                            blockUser();
+                            setBlockedByUser(false);
+                            api.post("/unblock/" + targetUser.id, {}, { withCredentials: true });
                           }}
                           className="text-red-400 flex items-center justify-center hover:bg-compBg/30 gap-1 py-2 px-4"
                         >
@@ -365,12 +343,9 @@ const Chat = () => {
                       ) : (
                         <li
                           onClick={() => {
-                            setBlocked(true);
-                            api.post(
-                              "/block/" + targetUser.id,
-                              {},
-                              { withCredentials: true }
-                            );
+                            blockUser();
+                            setBlockedByUser(true);
+                            api.post("/block/" + targetUser.id, {}, { withCredentials: true });
                           }}
                           className="text-red-600 flex items-center justify-center hover:bg-compBg/30 gap-1 py-2 px-4"
                         >
@@ -396,8 +371,7 @@ const Chat = () => {
                     >
                       <ChatBubble type="sender" message={message} />
                     </div>
-                  ) : i + 1 < arr.length &&
-                    arr[i + 1].recipient_id == targetUser.id ? (
+                  ) : i + 1 < arr.length && arr[i + 1].recipient_id == targetUser.id ? (
                     <div
                       key={message.id ?? message.tempId}
                       className="self-start gap-1  break-all wrap-anywhere text-wrap flex flex-row bg-neon/[55%] text-white px-4 py-2 rounded-2xl rounded-tl-sm rounded-bl-sm"
@@ -421,8 +395,7 @@ const Chat = () => {
                   >
                     <ChatBubble type="recipient" message={message} />
                   </div>
-                ) : i + 1 < arr.length &&
-                  arr[i + 1].recipient_id != targetUser.id ? (
+                ) : i + 1 < arr.length && arr[i + 1].recipient_id != targetUser.id ? (
                   <div
                     id={!message.isRead ? "message" : ""}
                     key={message.id}
@@ -444,14 +417,15 @@ const Chat = () => {
               )}
             </div>
             <div className="bg-compBg/20 h-[95px] items-center flex px-5 justify-between">
-              {blocked ? (
+              {blockedbyUser ? (
                 <div className="flex items-center justify-center flex-col text-white w-full">
-                  <h3 className="font-medium text-center text-[20px]">
-                    You've blocked this user
-                  </h3>
-                  <p className="text-white/50 text-[15px]">
-                    You can't send or receive messages until you unblock them
-                  </p>
+                  <h3 className="font-medium text-center text-[20px]">You've blocked this user.</h3>
+                  <p className="text-white/50 text-[15px]">You can't send or receive messages until you unblock them</p>
+                </div>
+              ) : blockedbyOther ? (
+                <div className="flex items-center justify-center flex-col text-white w-full">
+                  <h3 className="font-medium text-center text-[20px]">You can't message this user.</h3>
+                  <p className="text-white/50 text-[15px]">Messaging is unavailable</p>
                 </div>
               ) : (
                 <div className="flex p-1 flex-row bg-neon/[35%] items-center pr-4 w-full rounded-full">
@@ -470,9 +444,7 @@ const Chat = () => {
         ) : (
           <div className="flex gap-3 flex-col justify-center w-full h-full items-center">
             <img src="/src/assets/chat_bg.png" className="w-2/3" />
-            <h2 className="text-white text-[30px] text-center font-semibold">
-              Select a conversation to start chatting ✨
-            </h2>
+            <h2 className="text-white text-[30px] text-center font-semibold">Select a conversation to start chatting ✨</h2>
           </div>
         )}
       </div>
